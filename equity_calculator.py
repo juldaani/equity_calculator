dd573bbc6dd2a388afc9c67155839947e2e07ba3
juldaani <juho.uusiluomalahti@gmail.com>
2019-03-12 23:18:41 +0200

    equity calculator

--- equity_calculator.py
+++ equity_calculator.py
@@ -17,133 +17,70 @@ from equity_calculator.params import LUT_nChooseK_2cards, holeCardEquities
 
 
 
-
-
-# %%
-
-
-cards = np.random.choice(52, size=7, replace=0)
-holeCards = cards[:2]
-flopCards = cards[2:5]
-turnCards = cards[2:6]
-riverCards = cards[2:7]
-
-
-# Sort hole cards because otherwise lookup will fail
-if(holeCards[0] > holeCards[1]):
-    holeCards[0], holeCards[1] = holeCards[1], holeCards[0]
-
-# Preflop equity
-tmp0 = LUT_nChooseK_2cards[holeCards[0],0]
-tmp1 = LUT_nChooseK_2cards[holeCards[1],1]
-ind = tmp0 + tmp1
-holeCardEquity = holeCardEquities[ind]
-
-
-
-# Remove hole and board cards from the deck
-boardCards = flopCards
-mask = np.ones(52, dtype=np.bool_)
-mask[holeCards] = 0
-mask[boardCards] = 0
-deck = np.arange(52)[mask]
-np.random.shuffle(deck)
-
-# %%
-
-curCards, opponentCards = np.zeros(7, dtype=np.uint8), np.zeros(7, dtype=np.uint8)
-
-counter, wins = 0, 0
-for i1 in range(len(deck)):
-    c1 = deck[i1]
+@jit(nopython=True, cache=True, fastmath=True, nogil=True)
+def computeEquity(holeCards, boardCards, nIters):
+    # Remove hole and board cards from the deck
+    mask = np.ones(52, dtype=np.bool_)
+    mask[holeCards] = 0
+    mask[boardCards] = 0
+    deck = np.arange(52)[mask]
+    nCardsDeck = len(deck)
     
-    for i2 in range(i1+1,len(deck)):
-        c2 = deck[i2]
+    # Run simulations
+    wins = 0
+    curCards, opponentCards = np.zeros(7, dtype=np.uint8), np.zeros(7, dtype=np.uint8)
+    for k in range(nIters):
+        # Draw random cards from the deck
+        for t in range(7-len(boardCards)):
+            rndIdx = np.random.randint(t,nCardsDeck)
+            opponentCards[t] = deck[rndIdx]
+            deck[t],deck[rndIdx] = deck[rndIdx],deck[t]
+        
+        opponentCards[-len(boardCards):] = boardCards
+        
+        curCards[:5] = opponentCards[2:]    # Board cards
+        curCards[-2:] = holeCards
     
-        for i3 in range(i2+1,len(deck)):
-            c3 = deck[i3]
-            
-            for i4 in range(i3+1,len(deck)):
-                c4 = deck[i4]
-                
-                opponentCards[0], opponentCards[1], opponentCards[2], \
-                    opponentCards[3], opponentCards[4], opponentCards[5], opponentCards[6] = \
-                    boardCards[0],boardCards[1],boardCards[2],c1,c2,c3,c4
-                
-                curCards[0], curCards[1], curCards[2], curCards[3], \
-                    curCards[4], curCards[5], curCards[6] = \
-                    holeCards[0],holeCards[1],boardCards[0],boardCards[1],boardCards[2],c1,c2
-                
-                rankCur = evaluator(curCards, ranks_7cards, LUT_nChooseK_7cards)
-                rankOpponent = evaluator(opponentCards, ranks_7cards, 
-                                         LUT_nChooseK_7cards)
-                
-                isWin = int(rankCur < rankOpponent)
-                wins += isWin
-
-                counter += 1
-                
-equity = wins / counter
-
-print(equity)
-
-# %%
-
-pp = 10000
-
-counter, wins = 0, 0
-for i in range(pp):
-    idx = np.random.choice(47, size=4, replace=0)
+        rankCur = evaluator(curCards, ranks_7cards, LUT_nChooseK_7cards)
+        rankOpponent = evaluator(opponentCards, ranks_7cards, LUT_nChooseK_7cards)
     
-    c1,c2,c3,c4 = deck[idx[0]],deck[idx[1]],deck[idx[2]],deck[idx[3]]
+        isWin = int(rankCur < rankOpponent)
+        wins += isWin
     
-    opponentCards[0], opponentCards[1], opponentCards[2], \
-                    opponentCards[3], opponentCards[4], opponentCards[5], opponentCards[6] = \
-                    boardCards[0],boardCards[1],boardCards[2],c1,c2,c3,c4
-                
-    curCards[0], curCards[1], curCards[2], curCards[3], \
-        curCards[4], curCards[5], curCards[6] = \
-        holeCards[0],holeCards[1],boardCards[0],boardCards[1],boardCards[2],c1,c2
+    return wins / nIters
+
+
+@jit(nopython=True, parallel=True, fastmath=True, nogil=True)
+def computeEquities(holeCards, boardCards):
     
-    rankCur = evaluator(curCards, ranks_7cards, LUT_nChooseK_7cards)
-    rankOpponent = evaluator(opponentCards, ranks_7cards, 
-                             LUT_nChooseK_7cards)
+    preflopEquities = np.zeros(len(holeCards), dtype=np.float32)
+    flopEquities = np.zeros(len(holeCards), dtype=np.float32)
+    turnEquities = np.zeros(len(holeCards), dtype=np.float32)
+    riverEquities = np.zeros(len(holeCards), dtype=np.float32)
     
-    isWin = int(rankCur < rankOpponent)
-    wins += isWin
-
-    counter += 1
-
-equity = wins / counter
-print(equity)
-
-
-# 0.7466
-
-
-# %%
-
-def convertIntToCard(ints):
-    return [intToCard[intt] for intt in ints]
-
-import itertools
-
-holeCardCombs = np.array(list(itertools.combinations(np.arange(52), 2)))
-
-# %%
-
-ii = 645
-
-cur = holeCardCombs[ii]
-cur = [0+13*1,13*3]
-
-tmp0 = LUT_nChooseK_2cards[cur[0],0]
-tmp1 = LUT_nChooseK_2cards[cur[1],1]
-ind = tmp0 + tmp1
+    for i in prange(len(holeCards)):
+        curHoleCards = holeCards[i]
+        curBoardCards = boardCards[i]
+        
+        holeCard1 = curHoleCards[0]
+        holeCard2 = curHoleCards[1]
+        
+        # Sort hole cards because otherwise lookup will fail
+        if(holeCard1 > holeCard2):
+            holeCard1, holeCard2 = holeCard2, holeCard1
+        
+        # Preflop equity
+        ind = LUT_nChooseK_2cards[holeCard1,0] + LUT_nChooseK_2cards[holeCard2,1]
+        preflopEquities[i] = holeCardEquities[ind]
+    
+        # Flop equity
+        flopEquities[i] = computeEquity(curHoleCards, curBoardCards[:3], 10000)
+        turnEquities[i] = computeEquity(curHoleCards, curBoardCards[:4], 1500)
+        riverEquities[i] = computeEquity(curHoleCards, curBoardCards, 1000)
+        
+    return preflopEquities, flopEquities, turnEquities, riverEquities
 
 
-print(holeCardEquities[ind])
-print(convertIntToCard(cur))
 
 
 
